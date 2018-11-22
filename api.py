913fd8e39c928c16185984cb1ae635d2eddf2a1d
Claudio Freitas <claudiomfreitas@gmail.com>
2018-11-22 01:06:18 +0000

    New port and bind address

--- api.py
+++ api.py
@@ -3,9 +3,8 @@ import flask
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Useless API</h1><p>I just wasted resources.</p>"
 
-app.run()
+app.run(host='0.0.0.0',port=8080)
